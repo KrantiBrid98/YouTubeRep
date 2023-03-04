9e4d6142b54eefca7f75005d3ca8fd201b1652b0
KrantiBrid98 <krantibrid@gmail.com>
2023-03-04 16:49:56 +0530

    Updates

---
+++ precache-manifest.955555e1a865663b78ac54a305d0f58c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646f75afdf67a6cebd45eee2de96eb33",
+    "url": "/YouTubeRep/index.html"
+  },
+  {
+    "revision": "463805cd2c2199adcd36",
+    "url": "/YouTubeRep/static/css/main.b515364b.chunk.css"
+  },
+  {
+    "revision": "60c4d44fd88b1ec22ec4",
+    "url": "/YouTubeRep/static/js/2.008cdc52.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/YouTubeRep/static/js/2.008cdc52.chunk.js.LICENSE"
+  },
+  {
+    "revision": "463805cd2c2199adcd36",
+    "url": "/YouTubeRep/static/js/main.8551fe25.chunk.js"
+  },
+  {
+    "revision": "fe9e5c9f11137b9431a6",
+    "url": "/YouTubeRep/static/js/runtime-main.00a90102.js"
+  }
+]);
